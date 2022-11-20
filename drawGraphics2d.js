c6143ad0a66c53bcf2889357c4ffcd9f19ac60b8
Haspeldor <Haspeldor@gmail.com>
2022-11-20 15:14:29 +0100

    adding arrow, rectangle, polygon, reworking lines

--- drawGraphics2d.js
+++ drawGraphics2d.js
@@ -1,9 +1,6 @@
 //TODO: error handling?
-//TODO: no scaled coords yet
-//TODO: size scaling board.canvasWidth
-//TODO: dynamic coords problematic
-//TODO: dynamic interactions
-//TODO: curve instead of linie
+//TODO: no scaled coords yet -> point size?
+//TODO: some objects draggable
 
 function drawGraphics2d(id, json) {
     var boundingBox = json.extent;
@@ -38,7 +35,12 @@ function drawGraphic(board, json) {
         case "point":
             drawPoint(board, json, args);
             break;
+        case "arrow":
+            args.arrow = true;
+            drawLine(board, json, args);
+            break;
         case "line":
+            args.arrow = false;
             drawLine(board, json, args);
             break;
         case "disk":
@@ -67,7 +69,7 @@ function drawPoint(board, json, args) {
             fillColor: args.color,
             strokeOpacity: json.opacity,
             fillOpacity: json.opacity,
-            size: json.pointSize,
+            size: board.canvasWidth * json.pointSize / 2,
         });
     }
 }
@@ -85,10 +87,8 @@ function drawCircle(board, json, args) {
     }
 }
 
-function drawLine(board, json, args) {
+function drawLineSegmented(board, json, args) {
     for (index = 1; index < args.coords.length; index++) {
-        //TODO: line between dynamic points instead of fixed coords?
-        //TODO: additional directives: width, dashed, gap
         board.create("line", [args.coords[index], args.coords[index - 1]], {
             straightFirst: false,
             straightLast: false,
@@ -97,34 +97,47 @@ function drawLine(board, json, args) {
     }
 }
 
+function drawLine(board, json, args) {
+    //TODO: line between dynamic points instead of fixed coords?
+    //TODO: additional directives: width, dashed, gap
+    var newCoords = convertCoordsCurve(args.coords);
+
+    board.create("curve", newCoords, {
+        lastArrow: args.arrow,
+        strokeColor: args.color,
+        strokeOpacity: json.opacity,
+    });
+}
+
 function drawPolygon(board, json, args) {
-    //TODO: point creation necessary?
     board.create("polygon", args.coords, {
         strokeColor: args.color,
         fillColor: args.color,
         strokeOpacity: json.opacity,
         fillOpacity: json.opacity,
         fixed: true,
+        vertices: {visible:false}
     });
 }
 
 function drawRectangle(board, json, args) {
-    //TODO: polygon cant be fixed, so the points can be moved freely?
-    var start, end;
-    if (args.coords.length == 1) {
-        start = args.coords[0];
-        args.coords = [
-            start,
-            [start[0] + 1, start[1]],
-            [start[0] + 1, start[1] + 1],
-            [start[0], start[1] + 1],
-        ];
-    } else if (args.coords.length == 2) {
-        start = args.coords[0];
-        end = args.coords[1];
-        args.coords = [start, [start[0], end[1]], end, [end[0], start[1]]];
-    }
-    drawPolygon(board, json, args);
+    var start, end, p1, p2, p3, p4;
+    start = args.coords[0];
+    if (args.coords.length == 1) end = [start[0]+1, start[1]+1]
+    else if (args.coords.length == 2) end = args.coords[1]; 
+
+    p1 = board.create('point', [start[0], start[1]], {visible:false});
+    p2 = board.create('point', [end[0], end[1]], {visible:false});
+    p3 = board.create('point', [function(){return p1.X()},function(){return p2.Y()}], {visible:false});
+    p4 = board.create('point', [function(){return p2.X()},function(){return p1.Y()}], {visible:false});
+
+    board.create("polygon", [p1,p3,p2,p4], {
+        strokeColor: args.color,
+        fillColor: args.color,
+        strokeOpacity: json.opacity,
+        fillOpacity: json.opacity,
+        fixed: true,
+    });
 }
 
 function calculateFoci(radiusX, radiusY, coords) {
@@ -165,6 +178,16 @@ function convertCoords(coords) {
     return newCoords;
 }
 
+function convertCoordsCurve(coords) {
+    var x = [];
+    var y = [];
+    for (key in coords) {
+        x[key] = coords[key][0];
+        y[key] = coords[key][1];
+    }
+    return [x,y];
+}
+
 function testRun() {
     drawGraphics2d("graphics2d", {
         elements: [
@@ -187,7 +210,7 @@ function testRun() {
             {
                 type: "line",
                 color: [1.0, 0.5, 0.0],
-                opacity: 1.0,
+                opacity: 0.6,
                 coords: [
                     [[1.0, 1.0]],
                     [[3.0, 1.0]],
@@ -200,19 +223,19 @@ function testRun() {
                 color: [0.7, 1.0, 0.0],
                 coords: [[[0, 0]], [[1, 1]], [[2, 2]], [[3, 3]]],
                 opacity: 0.5,
-                pointSize: 3,
+                pointSize: 0.005,
             },
             {
                 type: "rectangle",
                 color: [0.0, 0.5, 1.0],
                 opacity: 1.0,
-                coords: [[[2.0, -3.0]], [[4.0, -6.0]]],
+                coords: [[[2.0, -5.0]], [[4.0, -2.0]]],
             },
             {
                 type: "arrow",
-                color: [1.0, 0.5, 0.0],
+                color: [0.2, 0.0, 1.0],
                 opacity: 1.0,
-                coords: [[[0.0, 0.0]], [[1.0, 1.0]]],
+                coords: [[[0.0, 0.0]], [[-4.0, 3.0]]],
             },
             {
                 type: "polygon",
